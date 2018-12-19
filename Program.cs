72c3968b01dc3af6c76bdf3aebdef738e98a86d4
A4L <45801468+Adventure4Life@users.noreply.github.com>
2018-12-19 13:11:06 +1100

    Video 5/17
    Working through the Tutorial

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Hyperion
         static void Main(string[] args)
         {
 
-            Console.WriteLine("This is the start.");
+            Console.WriteLine("This is the start!");
             Console.ReadKey();
         }
     }
